3ba820f0c5f6c460f17a6319461566ce44e8d1f0
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-10-13 23:24:07 +0330

    make login page persian && add sweet alert

--- auth/script.js
+++ auth/script.js
@@ -1,10 +1,20 @@
-const usernameInput = document.querySelector('#username');
-const passwordInput = document.querySelector('#password');
-const submitButton = document.querySelector('#submit');
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const submitButton = document.querySelector("#submit");
 
-submitButton.addEventListener('click', function ()
-{
-    console.log(usernameInput. value + " && " + passwordInput.value );
-    if (usernameInput.value === 'admin' && passwordInput.value === 'admin')
-        window.location.href = "/users";
+submitButton.addEventListener("click", function () {
+  if (usernameInput.value === "admin" && passwordInput.value === "admin") {
+    window.location.href = "/users";
+  } else {
+    let message = "نام کاربری یا رمز عبور اشتباه است، لطفا مجددا تلاش کنید.";
+    let footer = "با احترام، تیم پنج فرانت اند";
+
+    Swal.fire({
+      icon: "error",
+      title: "توجه",
+      text: message,
+      footer: footer,
+      confirmButtonText: "متوجه شدم",
+    });
+  }
 });
