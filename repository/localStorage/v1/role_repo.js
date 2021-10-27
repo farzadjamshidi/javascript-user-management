d832751dbc428ec590d21a8c8f8ed99a2afac2a6
farzadjamshidi <farzadjamshidicom@yahoo.com>
2021-10-28 01:40:01 +0300

    add role repository

---
+++ repository/localStorage/v1/role_repo.js
@@ -0,0 +1,55 @@
+var localStorageKey = 'roles';
+
+function _setRoles(roles = [])
+{
+    localStorage.setItem(localStorageKey, JSON.stringify(roles));
+}
+
+function getModel()
+{
+    return {
+        id: "1",
+        name: "admin",
+    };
+}
+function getRoles()
+{
+    return JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+}
+
+function getRoleById(roleId)
+{
+    return getRoles().find(role => role.id === roleId);
+}
+
+function deleteRole(roleId)
+{
+    var roles = getRoles();
+    var deletedRoleIndex = getRoles().findIndex(role => role.id === roleId);
+    roles.splice(deletedRoleIndex, 1);
+    _setRoles(roles);
+}
+
+function createRole(newRole)
+{
+    var roles = getRoles();
+    roles.push(newRole);
+    _setRoles(roles);
+}
+
+function updateRole(updatedRole)
+{
+    var roles = getRoles();
+    var updatedRoleIndex = getRoles().findIndex(role => role.id === updatedRole.id);
+    roles[updatedRoleIndex] = updatedRole;
+    _setRoles(roles);
+}
+
+var roleRepo = {
+    getModel: getModel,
+    getRoles: getRoles,
+    getRoleById: getRoleById,
+    deleteRole: deleteRole,
+    createRole: createRole,
+    updateRole: updateRole
+};
