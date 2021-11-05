c12b83f416fb3d973520a09ef56492aa18004e05
farzadjamshidi <farzadjamshidicom@yahoo.com>
2021-11-05 14:26:14 +0300

    add user profile modal to navbar

--- layout/load-layout.js
+++ layout/load-layout.js
@@ -16,6 +16,18 @@
                 window.location.href = "/";
             });
 
+            document.querySelector('#user-drop-down-profile').addEventListener('click', function ()
+            {
+                showUsersListModal();
+                afterShownUsersListModal(loggedInUser);
+            });
+
+            document.querySelector('#user-drop-down-change-password').addEventListener('click', function ()
+            {
+                authService.removeLoggedInUser();
+                window.location.href = "/";
+            });
+
             const loggedInUser = authService.getLoggedInUser();
             document.querySelector('#logged-in-user-full-name').innerHTML = `${ loggedInUser.firstName } ${ loggedInUser.lastName }`;
         });
@@ -29,4 +41,75 @@
         {
             document.querySelector("sidebar").innerHTML = data;
         });
+
+
+    function showUsersListModal()
+    {
+        document.getElementsByClassName('profile-modal')[0].classList.add('show');
+    }
+
+    function hideUsersListModal()
+    {
+        document.getElementsByClassName('profile-modal')[0].classList.remove('show');
+    }
+
+    function afterShownUsersListModal(loggedInUser)
+    {
+        document.querySelector('#profile-modal-close-btn').addEventListener('click', function ()
+        {
+            hideUsersListModal();
+        });
+
+        const profileModalContainer = document.querySelector('.profile-modal-container');
+
+        profileModalContainer.innerHTML = `
+            <div>
+            <label>Username:</label>
+            <span>${ loggedInUser.username }</span>
+            </div>
+
+            <div>
+            <label>First Name:</label>
+            <span>${ loggedInUser.firstName }</span>
+            </div>
+
+            <div>
+            <label>Last Name:</label>
+            <span>${ loggedInUser.lastName }</span>
+            </div>
+
+            <div>
+            <label>National Code:</label>
+            <span>${ loggedInUser.nationalCode }</span>
+            </div>
+
+            <div>
+            <label>Birth Date:</label>
+            <span>${ loggedInUser.birthDate }</span>
+            </div>
+
+            <div>
+            <label>City:</label>
+            <span>${ loggedInUser.city }</span>
+            </div>
+
+            <div>
+            <label>Mobile:</label>
+            <span>${ loggedInUser.mobile }</span>
+            </div>
+   
+            <div>
+            <label>Role:</label>
+            <span>${ loggedInUser.role }</span>
+            </div>
+
+            <div class="address">
+            <label>Address:</label>
+            <address>${ loggedInUser.address }</address>
+            </div>
+        `;
+
+
+    }
+
 })()
